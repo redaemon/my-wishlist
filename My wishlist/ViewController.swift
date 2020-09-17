44e23e1fdc18d8415890ebec1d7bd7e924276107
Dima <dimapda97@gmail.com>
2020-09-17 16:11:22 +0300

    Initial Commit

---
+++ My wishlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My wishlist
+//
+//  Created by Дмитрий on 17.09.2020.
+//  Copyright © 2020 NoCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
